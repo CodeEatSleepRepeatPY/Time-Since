2754377455f79fdbd50d4a885540341b002f3c4d
Freyja Kristjanson <kristjaf@myumanitoba.ca>
2022-02-26 16:14:40 +0000

    Create project structure

---
+++ app/src/test/java/comp3350/timeSince/tests/AllTests.java
@@ -0,0 +1,17 @@
+package comp3350.timeSince.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+
+})
+public class AllTests
+{
+
+}
