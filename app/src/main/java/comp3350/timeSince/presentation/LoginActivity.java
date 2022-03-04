96b63494200b894e8ec38738d2178643907c67dc
Freyja Kristjanson <kristjaf@myumanitoba.ca>
2022-03-04 05:56:54 +0000

    Automate tests

--- app/src/main/java/comp3350/timeSince/presentation/LoginActivity.java
+++ app/src/main/java/comp3350/timeSince/presentation/LoginActivity.java
@@ -1,17 +1,11 @@
 package comp3350.timeSince.presentation;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import comp3350.timeSince.R;
 
 public class LoginActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
