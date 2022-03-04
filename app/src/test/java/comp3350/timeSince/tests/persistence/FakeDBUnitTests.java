b7a618da2ee34773c7b09b11193f595ce73e930c
group2project <goreliks@myumanitoba.ca>
2022-03-04 12:19:55 -0600

    adding UserDSOs to the database in the constructor, and adjusted FakeDBUnitTests accordingly

--- app/src/test/java/comp3350/timeSince/tests/persistence/FakeDBUnitTests.java
+++ app/src/test/java/comp3350/timeSince/tests/persistence/FakeDBUnitTests.java
@@ -32,11 +32,14 @@ public class FakeDBUnitTests {
     @Test
     public void createDatabaseTest(){
         assertNotNull("Newly created database object should not be null", database);
-        assertEquals("The database should be empty",  0, database.getUsers().size());
+        assertEquals("The database should have 3 users in it",  3, database.getUsers().size());
     }
 
     @Test
     public void emptyDatabaseTest(){
+        database.removeUser(user1);
+        database.removeUser(user2);
+        database.removeUser(user3);
         assertEquals("The database should be empty", 0, database.getUsers().size());
     }
 
@@ -46,17 +49,11 @@ public class FakeDBUnitTests {
         database.addUser(user2);
         database.addUser(user3);
 
-        assertEquals("Size of database should be 3", 3, database.getUsers().size());
+        assertEquals("Size of database should be 6", 6, database.getUsers().size());
     }
 
     @Test
     public void removeUserTest(){
-        database.removeUser(user1);
-        assertEquals("Size of database should be 0", 0, database.getUsers().size());
-
-        database.addUser(user1);
-        database.addUser(user2);
-        database.addUser(user3);
 
         database.addEvent(user2, event2);
         database.addEvent(user3, event3);
@@ -76,31 +73,24 @@ public class FakeDBUnitTests {
 
     @Test
     public void addEventTest(){
-        database.addUser(user1);
-        database.addUser(user2);
-        database.addUser(user3);
-
         database.addEvent(user1, event1);
         database.addEvent(user2, event2);
         database.addEvent(user3, event3);
 
-        assertEquals("user1 should have 1 event", 1, user1.getUserEvents().size());
-        assertEquals("user2 should have 1 event", 1, user2.getUserEvents().size());
-        assertEquals("user3 should have 1 event", 1, user3.getUserEvents().size());
+        assertEquals("user1 should have 1 event", 1, database.getUser(user1.getUuid()).getUserEvents().size());
+        assertEquals("user2 should have 1 event", 1, database.getUser(user2.getUuid()).getUserEvents().size());
+        assertEquals("user3 should have 1 event", 1, database.getUser(user3.getUuid()).getUserEvents().size());
 
-        assertEquals("event1 object should have the name 'event1'", "event1", user1.getUserEvents().get(0).getName());
-        assertEquals("event2 object should have the name 'event2'", "event2", user2.getUserEvents().get(0).getName());
-        assertEquals("event3 object should have the name 'event3'", "event3", user3.getUserEvents().get(0).getName());
+        assertEquals("event1 object should have the name 'event1'", "event1", database.getUser(user1.getUuid()).getUserEvents().get(0).getName());
+        assertEquals("event2 object should have the name 'event2'", "event2", database.getUser(user2.getUuid()).getUserEvents().get(0).getName());
+        assertEquals("event3 object should have the name 'event3'", "event3", database.getUser(user3.getUuid()).getUserEvents().get(0).getName());
     }
 
     @Test
     public void removeEventTest(){
-        database.addUser(user1);
-        database.addUser(user2);
-        database.addUser(user3);
 
         database.removeEvent(user1, event1);
-        assertEquals("user1 should have 0 events", 0, user1.getUserEvents().size());
+        assertEquals("user1 should have 0 events", 0, database.getUser(user1.getUuid()).getUserEvents().size());
 
         database.addEvent(user1, event1);
         database.addEvent(user2, event2);
@@ -109,42 +99,46 @@ public class FakeDBUnitTests {
         database.removeEvent(user1, event1);
         database.removeEvent(user3, event3);
 
-        assertEquals("user1 should have 0 events", 0, user1.getUserEvents().size());
-        assertEquals("user2 should still have 1 event", 1, user2.getUserEvents().size());
-        assertEquals("user3 should have 0 events", 0, user3.getUserEvents().size());
+        assertEquals("user1 should have 0 events", 0, database.getUser(user1.getUuid()).getUserEvents().size());
+        assertEquals("user2 should still have 1 event", 1, database.getUser(user2.getUuid()).getUserEvents().size());
+        assertEquals("user3 should have 0 events", 0, database.getUser(user3.getUuid()).getUserEvents().size());
 
         database.removeEvent(user2, event2);
-        assertEquals("user2 should have 0 events", 0, user2.getUserEvents().size());
+        assertEquals("user2 should have 0 events", 0, database.getUser(user2.getUuid()).getUserEvents().size());
 
         database.removeEvent(user2, event2);
-        assertEquals("user2 should still have 0 events", 0, user2.getUserEvents().size());
+        assertEquals("user2 should still have 0 events", 0, database.getUser(user2.getUuid()).getUserEvents().size());
     }
 
     @Test
     public void getUsersTest(){
         database.addUser(user1);
-        assertEquals("getUsers().size() should return 1", 1, database.getUsers().size());
+        assertEquals("getUsers().size() should return 4", 4, database.getUsers().size());
 
         database.addUser(user2);
-        assertEquals("getUsers().size() should return 2", 2, database.getUsers().size());
+        assertEquals("getUsers().size() should return 5", 5, database.getUsers().size());
 
         database.addUser(user3);
-        assertEquals("getUsers().size() should return 3", 3, database.getUsers().size());
+        assertEquals("getUsers().size() should return 6", 6, database.getUsers().size());
     }
 
     @Test
     public void getUserTest(){
-        assertNull("database should not contain any users", database.getUser("uid1"));
-        assertNull("database should not contain any users", database.getUser("uid2"));
-        assertNull("database should not contain any users", database.getUser("uid3"));
+        assertNotNull("database should contain user1", database.getUser("uid1"));
+        assertNotNull("database should contain user2", database.getUser("uid2"));
+        assertNotNull("database should contain user3", database.getUser("uid3"));
 
-        database.addUser(user1);
-        database.addUser(user2);
-        database.addUser(user3);
+        UserDSO user4 = new UserDSO("uid4", UserDSO.MembershipType.free, "hash4");
+        UserDSO user5 = new UserDSO("uid5", UserDSO.MembershipType.free, "hash5");
+        UserDSO user6 = new UserDSO("uid6", UserDSO.MembershipType.free, "hash6");
 
-        assertNotNull("user1, with uid1, should be in the database", database.getUser("uid1"));
-        assertNotNull("user2, with uid2, should be in the database", database.getUser("uid2"));
-        assertNotNull("user3, with uid3, should be in the database", database.getUser("uid3"));
+        database.addUser(user4);
+        database.addUser(user5);
+        database.addUser(user6);
+
+        assertNotNull("user4, with uid4, should be in the database", database.getUser("uid4"));
+        assertNotNull("user5, with uid5, should be in the database", database.getUser("uid5"));
+        assertNotNull("user6, with uid6, should be in the database", database.getUser("uid6"));
 
         database.removeUser(user1);
         assertNull("database should not contain user1 so null should be returned", database.getUser(user1.getUuid()));
