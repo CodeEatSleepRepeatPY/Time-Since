b63b97d9b64295ce37dad63e8c029b4d8d48a943
freyjakristjanson <kristjaf@myumanitoba.ca>
2022-03-03 21:51:10 -0800

    Create app access to database

---
+++ app/src/main/java/comp3350/timeSince/application/Services.java
@@ -0,0 +1,18 @@
+package comp3350.timeSince.application;
+
+import comp3350.timeSince.persistence.FakeDatabase;
+import comp3350.timeSince.persistence.I_Database;
+
+public class Services {
+
+    private static I_Database databasePersistence = null;
+
+    public static synchronized I_Database getDatabase() {
+        if(databasePersistence == null) {
+            databasePersistence = new FakeDatabase();
+        }
+
+        return databasePersistence;
+    }
+
+}
