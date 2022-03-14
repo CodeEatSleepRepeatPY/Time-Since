fca3b26f69fcf849a53bab32f2d4aaecee6fa062
freyjakristjanson <kristjaf@myumanitoba.ca>
2022-03-14 18:50:05 -0500

    Create initial hsqldb files \n Basic imports, constructors, connection

---
+++ app/src/main/java/comp3350/timeSince/persistence/hsqldb/EventPersistenceHSQLDB.java
@@ -0,0 +1,27 @@
+package comp3350.timeSince.persistence.hsqldb;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventPersistenceHSQLDB {
+
+    private final String dbPath;
+
+    public EventPersistenceHSQLDB(final String dbPath) {
+        this.dbPath = dbPath;
+    }
+
+    private Connection connection() throws SQLException {
+        return DriverManager.getConnection("jdbc:hsqldb:file:" + dbPath + ";shutdown=true", "SA", "");
+    }
+
+
+}
+
+
