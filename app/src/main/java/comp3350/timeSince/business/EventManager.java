df424998c20683706a64998c267d618bd16dc491
group2project <goreliks@myumanitoba.ca>
2022-03-21 22:07:56 -0500

    modified the createOwnEvent method (it now fetches the user from the database)

--- app/src/main/java/comp3350/timeSince/business/EventManager.java
+++ app/src/main/java/comp3350/timeSince/business/EventManager.java
@@ -61,20 +61,23 @@ public class EventManager {
 
     public void createOwnEvent(UserDSO user, Date dueDate, String eventName, String tagName, boolean favorite){
         if(user != null) {
-            EventDSO event = new EventDSO(eventName); // set the name
-            EventLabelDSO eventTag = new EventLabelDSO(tagName); // set the tag name
+            UserDSO databaseUser = userPersistence.getUserByID(user.getID());
+            EventDSO event = new EventDSO(eventName); // create event object with specified name
+            EventLabelDSO eventTag = new EventLabelDSO(tagName); // create tag object with specified name
 
-            event.setID(id++);
-            event.setTargetFinishTime(dueDate); // set the due date
+            event.setID(id++); // set the ID of the event
+            event.setTargetFinishTime(dueDate); // set event's due date
             event.addTag(eventTag); // add tag
+            //user.getUserEvents().add(event); // add event to user's events list
+            databaseUser.getUserEvents().add(event); // add event to user's events list
 
-            eventPersistence.insertEvent(event); // add to user's events
-            eventLabelPersistence.insertEventLabel(eventTag); // insert the newly created event tag
+            eventPersistence.insertEvent(event); // insert event into the database
+            eventLabelPersistence.insertEventLabel(eventTag); // insert the newly created event tag into the database
 
             if (favorite) {
                 event.setFavorite();
-                userPersistence.getUserByID(user.getID()).addFavorite(event); // add to favourite's list
                 //user.getFavoritesList().add(event); // add to favourite's list
+                databaseUser.addFavorite(event); // add to favourite's list
             }
         }
     }
