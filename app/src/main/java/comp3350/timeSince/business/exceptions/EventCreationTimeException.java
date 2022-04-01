040a3650c284aaa3ceebf1d875531d7406592123
AndrewWongg <tand2@myumanitoba.ca>
2022-04-01 13:13:51 -0500

    remove event creation time exception

--- app/src/main/java/comp3350/timeSince/business/exceptions/EventCreationTimeException.java
+++
@@ -1,7 +0,0 @@
-package comp3350.timeSince.business.exceptions;
-
-public class EventCreationTimeException extends RuntimeException{
-    public EventCreationTimeException(String error){
-        super("The creation date for the event is before the current date!\n"+ error);
-    }
-}
