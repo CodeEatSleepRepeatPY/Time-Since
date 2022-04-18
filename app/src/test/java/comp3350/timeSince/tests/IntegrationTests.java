de589f69b60e4ad2fb9597b922a31cf573953041
freyjakristjanson <kristjaf@myumanitoba.ca>
2022-04-18 14:33:49 -0500

    Create UserEventManager tests

--- app/src/test/java/comp3350/timeSince/tests/IntegrationTests.java
+++ app/src/test/java/comp3350/timeSince/tests/IntegrationTests.java
@@ -3,7 +3,9 @@ package comp3350.timeSince.tests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import comp3350.timeSince.business.EventLabelManager;
 import comp3350.timeSince.tests.business.EventDisplayTest;
+import comp3350.timeSince.tests.business.EventLabelManagerTest;
 import comp3350.timeSince.tests.business.EventManagerTest;
 import comp3350.timeSince.tests.business.UserManagerTest;
 import comp3350.timeSince.tests.objects.EventsWithLabelsTest;
@@ -15,6 +17,7 @@ import comp3350.timeSince.tests.objects.UsersWithEventsTest;
         UsersWithEventsTest.class,
         UserManagerTest.class,
         EventManagerTest.class,
+        EventLabelManagerTest.class,
         EventDisplayTest.class
 })
 public class IntegrationTests {
